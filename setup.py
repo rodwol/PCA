08e142affc800723c7df4bf88b8c2e324967be53
rodwol <r.goniche@alustudent.com>
2025-06-11 10:42:12 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alumathp7',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    description='Matrix multiplication for different dimension matrices',
+    author='Peer Group 7',
+    license='MIT'
+)
+
